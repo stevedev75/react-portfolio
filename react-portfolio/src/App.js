39371c8aeba5a9a8810db0292e99ffbd16ef7149
Steve Dever <stephendever.cr@gmail.com>
2021-07-23 08:23:35 -0400

    add footer files

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,29 @@
-//import logo from './logo.svg';
-import './App.css';
+  
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/index";
+import Wrapper from "./components/Wrapper/index";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import ResumePage from "./pages/ResumePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename="/">
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/resume" component={ResumePage} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ react-portfolio/src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer class="page-footer fixed-bottom font-small special-color-blue pt-4"> 
+
+      <div class="footer-copyright text-center py-3">
+        © 2021 Copyright Stephen Dever
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
