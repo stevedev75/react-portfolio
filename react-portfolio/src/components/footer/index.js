e3bd756745261590383a08bc1d793daef4d103f6
Steve Dever <stephendever.cr@gmail.com>
2021-07-23 08:38:59 -0400

    add pages files, about, contact, portfolio and resumepage js

--- react-portfolio/src/components/footer/index.js
+++ react-portfolio/src/components/footer/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer class="page-footer fixed-bottom font-small special-color-blue pt-4"> 
+    <footer class="page-footer fixed-bottom font-small special-color-dark pt-4"> 
 
       <div class="footer-copyright text-center py-3">
         © 2021 Copyright Stephen Dever
-        </a>
+        
       </div>
     </footer>
   );
