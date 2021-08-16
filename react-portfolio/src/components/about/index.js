d16f963bac836d9548420cf3b99816cb645fd9d1
Steve Dever <stephendever.cr@gmail.com>
2021-08-16 10:50:14 -0400

    update about section

--- react-portfolio/src/components/about/index.js
+++ react-portfolio/src/components/about/index.js
@@ -16,16 +16,15 @@ const AboutMe = () => {
             </img>
           
           <p style={{ fontSize: "18px" }}>
-           Hello! My name is Stephen Dever, and I am currently a Full Stack Web Development student in
-           the University of Pennsylvania’s coding bootcamp. Previously, I worked in and around the higher education arena for over twenty years both at the university level as well as in the private sector, as an independent educational counselor. I hold a bachelor’s degree in English and a master’s degree in Education.
+           Hello! My name is Stephen Dever, and I am a Front End Web Developer and a graduate of the University of Pennsylvania’s Full Stack Web Development coding bootcamp. Previously, I worked in and around the higher education arena for over twenty years both at the university level as well as in the private sector, as an independent educational counselor. I hold a bachelor’s degree in English and a master’s degree in Education.
           </p>
           <p style={{ fontSize: "18px" }}>
             In my most recent roles, I was a power user of various educational technology products and 
-            platforms which piqued my interest in web development, eventually leading me to this exciting pivot. Now, as I am entering the “Career 2.0” phase of my life I couldn’t be more pleased about what I have already learned and what I will be learning during the rest of this six-month program.
+            platforms which piqued my interest in web development, eventually leading me to this exciting pivot. Now, as I am entering the “Career 2.0” phase of my life I couldn’t be more pleased.
           </p>
  	  <p style={{ fontSize: "18px" }}>
-            So far, we have had practical experience with HTML, CSS, JavaScript, jQuery, Bootstrap,
-            APIs, Node.js, express, MySQL, GitHub, Heroku, MondoDB, React and more. In addition, we will be continuing our "hands-on" studies and developing many other in-demand programming skills throughout the course of this bootcamp. Stay tuned as I will be updating both the content and the style of this web page frequently!
+           I have had practical experience with HTML, CSS, JavaScript, jQuery, Bootstrap,
+            APIs, Node.js, Express, MySQL, GitHub, GraphQL, Heroku, MondoDB, React and more. Stay tuned as I will be updating both the content and the style of this web page frequently!
           </p>
         </MDBCol>
       </MDBRow>
